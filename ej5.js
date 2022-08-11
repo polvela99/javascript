35bcdb46863c22f32d647b47c9138918b92e9806
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-11 13:44:18 +0200

    Add files via upload

---
+++ ej5.js
@@ -0,0 +1,13 @@
+console.log(numero(Math.round(Math.random())));
+
+function numero(number){
+    if (number == 0){
+        console.log(number);
+        return("cruz");
+    }else{
+        console.log(number);
+        return("cara");
+    }
+}
+
+
