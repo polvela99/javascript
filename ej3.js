501de71c8ba9cc288c62d89ad276efcc7beadcde
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-11 11:06:43 +0200

    Add files via upload

---
+++ ej3.js
@@ -0,0 +1,18 @@
+var nombre="Pol";
+const numero = 3.14;
+let calidad = false;
+function miNombre(){
+    var nombre = "Dembe";
+    console.log(nombre);
+}
+console.log(nombre);
+console.log(numero);
+console.log(calidad);
+
+console.log(typeof(nombre));
+console.log(typeof(numero));
+console.log(typeof(calidad));
+
+var nombre = "Bale";
+console.log(nombre);
+miNombre();
