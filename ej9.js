4949716620820aa81b56848c8913d43c490515a6
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-12 13:12:36 +0200

    Add files via upload

---
+++ ej9.js
@@ -0,0 +1,9 @@
+//parte1
+console.log(Math.round(Math.random()));
+
+//parte2
+var numero = 3.141516;
+console.log(Math.PI.toFixed(2));
+console.log(Math.PI*100); //314.159265358
+console.log(Math.round(Math.PI*100)); //314
+console.log(Math.round(Math.PI*1000)/1000); //3141.59265358 / 100 = 
