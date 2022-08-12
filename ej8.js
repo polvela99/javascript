496cd18b63470d862862108f67d1d4a418c98d25
polvela99 <109659466+polvela99@users.noreply.github.com>
2022-08-12 12:30:53 +0200

    Add files via upload

---
+++ ej8.js
@@ -0,0 +1,50 @@
+let numero = 4;
+
+switch(numero){
+    case 1:
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("marzo");
+        break;
+    case 4:
+        console.log("abril");
+        break;
+    case 5:
+        console.log("mayo");
+         break;
+    case 6:
+        console.log("junio");
+        break;
+    default:
+        console.error("ERROR: EL NUMERO INTRODUCIDO NO ES UN MES CORRECTO");
+}
+
+//PARTE 2
+let numero1 = "dos";
+
+    switch(numero1){
+        case "cinco":
+            console.log(5);
+            break;
+        case "cuatro":
+            console.log(4);
+            break;
+        case "tres":
+            console.log(3);
+            break;
+        case "dos":
+            console.log(2);
+            break;
+        case "uno":
+            console.log(1);
+            break;
+        default:
+            console.error("ERROR");
+    }
+
+
+
